86079da09e427df7d443ea99900afbf8b5be8b7e
Katharine <krsibbald@gmail.com>
2018-02-17 10:09:30 -0500

    Validations on card order

--- app/models/card_spot.rb
+++ app/models/card_spot.rb
@@ -2,5 +2,15 @@ class CardSpot < ApplicationRecord
   belongs_to :combo, counter_cache: true
   belongs_to :card
 
-  validates :combo, :card, :ord, presence: true
+  validates :combo, :card, presence: true
+  validates :ord, presence: true, uniqueness: { scope: :combo,  message: "is already taken for this combo" }
+  validate :must_be_sequential
+
+  private
+
+  def must_be_sequential
+    if ord > combo.card_spots.count + 1
+      errors[:ord] << " is invalid was #{ord} needs to be greater than #{combo.card_spots.count + 1}"
+    end
+  end
 end
