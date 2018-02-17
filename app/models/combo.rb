5549bedb5c924d2aea65e1daeb93e64a12254534
Katharine <krsibbald@gmail.com>
2018-02-16 21:55:31 -0500

    test for importing from cordova

--- app/models/combo.rb
+++ app/models/combo.rb
@@ -51,4 +51,64 @@ class Combo < ApplicationRecord
     end
     ans
   end
+
+  def self.import_from_cordova(combos_string)
+    #removes all white space, puts quotes before and after numbers
+    json_combos = combos_string.squish.delete(' ').gsub(/(\d+)/, '"\\1"')
+    combos_hash = JSON.parse json_combos
+
+    combos_hash.keys.each do |k1|
+      card1 = Card.find_by_game_id(k1)
+      combos_hash[k1].keys.each do |k2|
+        if k2 == '0'
+          combo = Combo.create(outcome: true)
+          CardSpot.create(combo: combo, card: card1, ord: 1)
+        else
+          card2 = Card.find_by_game_id(k2)
+          combos_hash[k1][k2].keys.each do |k3|
+            if k3 == '0'
+              combo = Combo.create(outcome: true)
+              CardSpot.create(combo: combo, card: card1, ord: 1)
+              CardSpot.create(combo: combo, card: card2, ord: 2)
+            else
+              card3 = Card.find_by_game_id(k3)
+              combos_hash[k1][k2][k3].keys.each do |k4|
+                if k4 == '0'
+                  combo = Combo.create(outcome: true)
+                  CardSpot.create(combo: combo, card: card1, ord: 1)
+                  CardSpot.create(combo: combo, card: card2, ord: 2)
+                  CardSpot.create(combo: combo, card: card3, ord: 3)
+                else
+                  card4 = Card.find_by_game_id(k4)
+                  combos_hash[k1][k2][k3][k4].keys.each do |k5|
+                    if k5 == '0'
+                      combo = Combo.create(outcome: true)
+                      CardSpot.create(combo: combo, card: card1, ord: 1)
+                      CardSpot.create(combo: combo, card: card2, ord: 2)
+                      CardSpot.create(combo: combo, card: card3, ord: 3)
+                      CardSpot.create(combo: combo, card: card4, ord: 4)
+                    else
+                      card5 = Card.find_by_game_id(k5)
+                      combos_hash[k1][k2][k3][k4][k5].keys.each do |k6|
+                        if k5 == '0'
+                          combo = Combo.create(outcome: true)
+                          CardSpot.create(combo: combo, card: card1, ord: 1)
+                          CardSpot.create(combo: combo, card: card2, ord: 2)
+                          CardSpot.create(combo: combo, card: card3, ord: 3)
+                          CardSpot.create(combo: combo, card: card4, ord: 4)
+                          CardSpot.create(combo: combo, card: card5, ord: 5)
+                        else
+                          puts "bad section of hash #{k1}:#{k2}:#{k3}:#{k4}:#{k5}"
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
--- spec/models/combo_spec.rb
+++ spec/models/combo_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Combo, type: :model do
+  let!(:card1) { create :card, game_id: 1 }
+  let!(:card2) { create :card, game_id: 2 }
+  let!(:card3) { create :card, game_id: 3 }
+
   describe "#export_as_hash" do
-    let!(:card1) { create :card, game_id: 1 }
-    let!(:card2) { create :card, game_id: 2 }
-    let!(:card3) { create :card, game_id: 3 }
+
     let!(:combo1) { create :combo, outcome: true }
     let!(:cs1) { create :card_spot, combo: combo1, card: card1, ord: 1 } 
     let!(:cs2) { create :card_spot, combo: combo1, card: card2, ord: 2 }
@@ -29,5 +31,24 @@ RSpec.describe Combo, type: :model do
       expect(Combo.export_as_hash).to eq ({1=>{2=>{3=>{0=>true}}},3=>{2=>{0=>true}}})
     end
     
+  end
+
+  describe "#import_from_cordova" do
+    it "one combo" do
+      #0 is last item in hand
+      combos_string = "{
+        1: { 3: { 0: true } }
+      }"
+      expect(CardSpot.count).to eq 0
+      expect(Combo.count).to eq 0 
+      Combo.import_from_cordova(combos_string)
+      expect(Combo.count).to eq 1
+      expect(CardSpot.count).to eq 2
+      first_combo = Combo.first
+
+
+      expect(CardSpot.where(combo: first_combo, ord: 1).first.card).to eq card1
+      expect(CardSpot.where(combo: first_combo, ord: 2).first.card).to eq card3
+    end
   end
 end
